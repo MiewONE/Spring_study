624151932eea2ee98c59de1365ec5f8362cc1630
MiewONE <dlsrk489@gmail.com>
2020-10-22 17:41:19 +0900

    upload

---
+++ pro24/src/com/spring/member/dao/MemberDAOImpl.java
@@ -0,0 +1,37 @@
+package com.spring.member.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.dao.DataAccessException;
+
+import com.spring.member.vo.MemberVO;
+
+
+public class MemberDAOImpl implements MemberDAO {
+	private SqlSession sqlSession;
+
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}//속성 sqlSession에 sqlSession 빈을 주입하기 위해 setter을 구현
+
+	@Override
+	public List selectAllMemberList() throws DataAccessException {
+		List<MemberVO> membersList = null;
+		membersList = sqlSession.selectList("mapper.member.selectAllMemberList");
+		//주입된 selSession 빈으로 selectList() 메서드를 호출. sql문의 id를 전달
+		return membersList;
+	}
+
+	@Override
+	public int insertMember(MemberVO memberVO) throws DataAccessException {
+		int result = sqlSession.insert("mapper.member.insertMember", memberVO);
+		return result;
+	}
+
+	@Override
+	public int deleteMember(String id) throws DataAccessException {
+		int result =  sqlSession.delete("mapper.member.deleteMember", id);
+		return result;
+	}
+}
